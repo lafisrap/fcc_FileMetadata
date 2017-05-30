b074804c8a795e4ebdc2b6da2690accba59610d1
Michael <michael@c2064.org>
2017-05-30 12:04:30 +0200

    Access control ...

--- app/server.js
+++ app/server.js
@@ -5,6 +5,12 @@ var express = require("express"),
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.post('/upload', upload.single('file'), function (req, res, next) {
   console.log(req);
   // req.file is the `avatar` file 
